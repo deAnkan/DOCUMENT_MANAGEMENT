644f4081c04f7bf63a1f07e8f5fb0e14a6cf3fd7
Bipasa Roy <bipasaroy453@gmail.com>
2025-12-12 13:39:46 +0530

    updated

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+const userSchema = new Schema(
+    {
+        name:
+        {
+            type: String,
+            required: true,
+
+        },
+        email:
+        {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password:
+        {
+            type: String,
+            required: true,
+        },
+        role:
+        {
+            type: String,
+            enum: ["HR", "accountant", "user"],
+            default:" user",
+  
+        },
+    },  {timestamp: true})
+
+export const User = mongoose.model("User", userSchema)
