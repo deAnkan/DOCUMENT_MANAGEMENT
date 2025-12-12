b69bdeb5c18196e52855ae9aafa4aa44d83b7f2e
Bipasa Roy <bipasaroy453@gmail.com>
2025-12-12 16:06:26 +0530

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from "../models/userModel.js";
+import User from "../models/User.js";
 import { response } from 'express';
 
 // Sign Up a new user
