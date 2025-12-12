d35cf50e6208013be3656d8da0736d4021a4eee7
Bipasa Roy <bipasaroy453@gmail.com>
2025-12-12 15:37:29 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import connectDB from "./db/index.db.js";
-import dotenv from "dotenv";
+// import connectDB from "./db/index.db.js";
+// import dotenv from "dotenv";
 
 
 
-dotenv.config({
-    path: './.env'
-})
+// dotenv.config({
+//     path: './.env'
+// })
 
-connectDB()
-    .then(() =>{
-        app.listen(process.env.PORT || 4000, () => {
-            console.log(`server is running at port : $${process.env.PORT}`);
-        })
-    })
-    .catch((err) => {
-        console.log("MONGO db connection failed !!", err);
-    })
+// connectDB()
+//     .then(() =>{
+//         app.listen(process.env.PORT || 4000, () => {
+//             console.log(`server is running at port : $${process.env.PORT}`);
+//         })
+//     })
+//     .catch((err) => {
+//         console.log("MONGO db connection failed !!", err);
+//     })
