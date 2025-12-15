cf6c9574a3d0cc2b465e74ca75e2b9d7b8b9ee16
deAnkan <deyankan154@gmail.com>
2025-12-15 14:57:25 +0530

    role checking included

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(String(process.env.MONGODB_URI));
         console.log("MongoDB connected successfully");
     } catch (err) {
         console.error("MongoDB connection failed:", err.message);
