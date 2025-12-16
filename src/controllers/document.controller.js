f3d48834661b5ff3923fcb28f6c1ab636ec0d04a
deAnkan <deyankan154@gmail.com>
2025-12-16 18:12:13 +0530

    api error fixing

--- src/controllers/document.controller.js
+++ src/controllers/document.controller.js
@@ -6,7 +6,7 @@ import { Document } from "../models/document.model.js";
 export const uploadDocument = async (req, res) => {
     try {
         
-        if(!req.file) {
+        if(!req.files) {
         return res.status(400).json({message: "No file uploaded"});
         
         }
@@ -18,7 +18,7 @@ export const uploadDocument = async (req, res) => {
         const document = await Document.create({
             user: req.user._id,
             fileUrl: result.secure_url,
-            publicId: result.public_id
+
         });
         res.status(201).json({message: "Document uploaded successfully", document});
 
@@ -33,78 +33,64 @@ export const uploadDocument = async (req, res) => {
 //hr approve/reject document
 
 export const hrActionDocument = async (req, res) => {
-   try {
-     const { documentId } = req.params;
-     const { action, message } = req.body;
- 
-     const document = await Document.findById(documentId);
- 
-     if (!document){
-         return res.status(404).json({message: "Document not found"});
- 
-     }
+    const { documentId } = req.params;
+    const { action, message } = req.body;
 
-     if (!["APPROVED", "REJECTED"].includes(action)) {
-  return res.status(400).json({ message: "Invalid action" });
-}
+    const document = await Document.findById(documentId);
 
- 
-     if (action === "APPROVED") {
-         document.status = "HR_APPROVED";
-         document.hrMessage = message || "APPROVED BY HR"
- 
-     }
- 
-     if (action === "REJECTED") {
-         document.status = "HR_REJECTED";
-         document.hrMessage = "REJECTED BY HR"
-     }
- 
-     document.hrActionAt = Date.now();
- 
-     await document.save();
-     res.status(200).json({message: "Document action completed successfully", document});
- } catch (error) {
-        res.status(500).json({message: "something went wrong"});
-    
-   }
+    if (!document){
+        return res.status(404).json({message: "Document not found"});
+
+    }
+
+    if (action === "APPROVED") {
+        document.status = "HR_APPROVED";
+        document.hrMessage = "APPROVED BY HR"
+
+    }
+
+    if (action === "REJECTED") {
+        document.status = "HR_REJECTED";
+        document.hrMessage = "REJECTED BY HR"
+    }
+
+    document.hrActionAt = Date.now();
+
+    await document.save();
+    res.status(200).json({message: "Document action completed successfully", document});
 };
 
 //accountant approve and release amont
 
 export const accountantDocument = async (req, res) => {
-   try {
-     const { documentId } = req.params;
-     const { amount, message } = req.body;
- 
-     const document = await Document.findById(documentId);
- 
-     if (!document){
-         return res.status(404).json({message: "Document not found"});
- 
-     }
- 
-     if(document.status !== "HR_APPROVED") {
-         return res.status(400).json({message: "Document not approved by HR"});
-     }
- 
-     else {
-         document.status = "ACCOUNTANT_AMOUNT_RELEASED";
-         document.accountantMessage = message || "AMOUNT RELEASED BY ACCOUNTANT";
-         document.amountReleased = amount;
-         document.accountantActionAt = Date.now();
- 
-         await document.save();
-         res.status(200).json({message: "Amount released successfully", document});
-     }
- 
- 
-     } catch (error) {
-        res.status(500).json({message: "something went wrong"});
-   }
-};
+    const { documentId } = req.params;
+    const { amount } = req.body;
 
-//resubmition after rejection
+    const document = await Document.findById(documentId);
+
+    if (!document){
+        return res.status(404).json({message: "Document not found"});
+
+    }
+
+    if(document.status !== "HR_APPROVED") {
+        return res.status(400).json({message: "Document not approved by HR"});
+    }
+
+    else {
+        document.status = "ACCOUNTANT_AMOUNT_RELEASED";
+        document.amountReleased = amount;
+        document.accountantMessage = "Amount released by accountant";
+        document.accountantActionAt = Date.now();
+
+        await document.save();
+        res.status(200).json({message: "Amount released successfully", document});
+    }
+
+
+}
+
+//resubmission after rejection
 
 export const resubmitDocument = async (req, res) => {
 
