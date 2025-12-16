48d34bde0878a6d257e9dae018dc09591bb78116
Bipasa Roy <bipasaroy453@gmail.com>
2025-12-16 11:51:12 +0530

    add document models at models

---
+++ src/models/document.model.js
@@ -0,0 +1,53 @@
+import mongoose, { Schema} from "mongoose";
+
+const documentSchema = new Schema({
+
+    user:
+       {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+       },
+    fileurl:
+         {
+            type: String,
+            required: true,
+         },
+    status:
+         {
+            type: String,
+            enum: ["PENDING", "HR_APPROVED", "HR_REJECTED", "ACCOUNTANT_APPROVED"],
+            default: "PENDING"
+         },
+    hrMessage:
+    {
+        type: String,
+
+    },
+    accountantMessage:
+    {
+        type: String
+    },
+    amountReleased:
+    {
+        type: Number,
+    },
+    submittedAt:
+    {
+        type: Date,
+        default: Date.now
+    },
+
+    hrActionAt:
+    {
+        type: Date
+    },
+    accontantActionAt:
+    {
+        type: Date
+    }
+
+
+})
+
+export const document = mongoose.model("Document", documentSchema)
