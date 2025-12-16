1107f0886c0377bef4d193c93fe109df18eb35ce
deAnkan <deyankan154@gmail.com>
2025-12-16 17:39:44 +0530

    authcontroller,fileuploadmiddleware,document routes,server.js

--- src/middleware/fileupload.middleware.js
+++ src/middleware/fileupload.middleware.js
@@ -3,6 +3,6 @@ import fileUpload from "express-fileupload";
 export const uploadMiddleware = fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+    limits: { fileSize: 100 * 1024 * 1024 }, // 100MB
     abortOnLimit: true
 });
